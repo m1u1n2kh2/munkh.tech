f5f220b1a3c0de302f8096a935c5123559e04d25
Munkh Altangerel <munkh@Munkh.local>
2019-02-08 18:09:14 -0800

    Updates

---
+++ precache-manifest.0bcb1e058a94fb1ca6b03d62c047e638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b313d3dafeddc11f2a8c",
+    "url": "/static/css/main.510c906b.chunk.css"
+  },
+  {
+    "revision": "b313d3dafeddc11f2a8c",
+    "url": "/static/js/main.b313d3da.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f21a678856af80e095ad317cd0f8fdec",
+    "url": "/index.html"
+  }
+];
